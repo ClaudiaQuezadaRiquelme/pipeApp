4521ecdb1bf2cbb2d40ebe27bded5b6f5995a026
Claudia Andrea Quezada Riquelme <cquezada@tisal.local>
2022-01-28 20:12:21 -0300

    i18nPlural Pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./no-comunes.component.css']
 })
 export class NoComunesComponent implements OnInit {
+
+  //i18nSelect
   nombre: string = "Clau Miaua";
   genero: string = 'femenino';
 
@@ -13,6 +15,15 @@ export class NoComunesComponent implements OnInit {
     'masculino': 'invitarlo',
     'femenino': 'invitarla',
     'neutro': 'invitarle'
+  }
+
+  //i18nPlural
+  clientes: string[] = ['María', 'Juan', 'Solange', 'Pascual'];
+
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando',
+    'other': 'tenemos # clientes esperando.'
   }
 
   constructor() { }
